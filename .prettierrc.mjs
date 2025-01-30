f53c55e86d936f919351ea0969a74eaeca43168c
LeeKangbok <pmmm114@gmail.com>
2025-01-30 19:47:33 +0900

    fix: Update VS Code and Prettier configuration
    - TypeScript, ESLint 및 Prettier 용 VSC 설정 추가
    - .vscode 추적을 제거하기 위해 .gitignore 업데이트
    - tailwindCSS prettier plugin 추가

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,3 +1,7 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
 const config = {
   plugins: ['prettier-plugin-tailwindcss'],
   tabWidth: 2,
@@ -5,6 +9,11 @@ const config = {
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 80,
+  /**
+   * TailwindCSS
+   * https://www.npmjs.com/package/prettier-plugin-tailwindcss#sorting-classes-in-function-calls
+   */
+  tailwindFunctions: ['cn'],
 };
 
 export default config;
