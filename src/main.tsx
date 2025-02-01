033b01a598ed3cba2df358b181fb872991c9fb8c
LeeKangbok <pmmm114@gmail.com>
2025-02-01 14:53:54 +0900

    fix: MSW 설정
    - onUnhandledRequest bypass하도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,9 @@ async function enableMocking() {
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  return worker.start();
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  });
 }
 
 enableMocking().then(() => {
