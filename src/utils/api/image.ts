9cda9396ff2d0873aea1444ecb5d5ac5f8e75755
LeeKangbok <pmmm114@gmail.com>
2025-02-02 13:09:52 +0900

    refactor: 랜덤 이미지 URL 생성 시 Math.random() 사용
    - getRandomInt 대신 Math.random() 메서드로 간소화

--- src/utils/api/image.ts
+++ src/utils/api/image.ts
@@ -1,5 +1,4 @@
 import { RANDOM_IMAGE_URL } from '@/const/external';
-import { getRandomInt } from '@/utils/core/number';
 
 interface IGetRandomImageParams {
   width: number;
@@ -15,9 +14,7 @@ interface IGetRandomImageParams {
  */
 export function getRandomImageUrl(params: IGetRandomImageParams): string {
   const { width, height, extension, isRandom = true } = params;
-  const randomSeed = isRandom
-    ? `?random=${getRandomInt({ min: 1, max: 1000 })}`
-    : '';
+  const randomSeed = isRandom ? `?random=${Math.random()}` : '';
   const url = `${RANDOM_IMAGE_URL}/${width}/${height}.${extension}${randomSeed}`;
 
   return url;
