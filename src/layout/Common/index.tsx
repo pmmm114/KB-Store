992647b96b8cfb4eac7bad108fdf5b91f35d2435
LeeKangbok <pmmm114@gmail.com>
2025-02-05 21:32:40 +0900

    feat: CommonLayout 헤더 및 스타일 추가
    - 공통 레이아웃에 헤더 컴포넌트 구현
    - Tailwind 클래스 적용을 위한 styles.ts 파일 추가

--- src/layout/Common/index.tsx
+++ src/layout/Common/index.tsx
@@ -1,9 +1,16 @@
 import { Suspense } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router';
 
+import { applyClass } from '@/utils/style/tailwind';
+
+import * as S from './styles';
+
 const CommonLayout = () => {
   return (
-    <div className="max-w-[390px] bg-black box-shadow-[0_4px_10px_0_rgba(39,56,85,.2),inset_2px_2px_0_0_#fff]">
+    <div className={applyClass(S.COMMON_LAYOUT_TAILWIND_CLASS.LAYOUT)}>
+      <header className={applyClass(S.COMMON_LAYOUT_TAILWIND_CLASS.HEADER)}>
+        <div>KB DEMO</div>
+      </header>
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
