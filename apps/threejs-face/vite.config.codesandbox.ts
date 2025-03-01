bd6cfab00e0f81bc138b8be3b7e8227abb37ecdc
LeeKangbok <pmmm114@gmail.com>
2025-03-01 20:47:55 +0900

    [ 3d-cartoon ] feat: threejs face 프로젝트 생성

---
+++ apps/threejs-face/vite.config.codesandbox.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
+  server: {
+    allowedHosts: true,
+  },
+});
