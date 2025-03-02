467e29654cfd8394934dccc0929988a1ab2ad265
LeeKangbok <pmmm114@gmail.com>
2025-03-02 11:52:51 +0900

    [ 3d-cartoon ] feat: Three.js와 React Three Fiber를 Threejs-face 프로젝트에 추가 (#13)
    - 캔버스와 지향성 조명으로 기본 3D 장면 데모 추가
    - useFrame 후크를 사용한 회전 애니메이션 추가

--- apps/threejs-face/src/App.tsx
+++ apps/threejs-face/src/App.tsx
@@ -1,7 +1,43 @@
+import { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Mesh } from 'three';
+
+function RotatingBox() {
+  // useRef로 mesh를 선택
+  const ref = useRef<Mesh>(null);
+
+  // 매 프레임마다 실행되는 훅
+  useFrame((state, delta) => {
+    // ref.current가 실제 THREE.Mesh 객체
+    if (ref.current) {
+      ref.current.rotation.x += delta;
+      ref.current.rotation.y += delta;
+    }
+  });
+
+  return (
+    <mesh ref={ref}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
 function App() {
   return (
-    <div>
-      <h1>Three.js Face</h1>
+    <div
+      style={{
+        height: '500px',
+      }}
+    >
+      <Canvas>
+        {/* 조명(예: AmbientLight, DirectionalLight 등) */}
+        <directionalLight position={[1, 1, 1]} />
+
+        {/* 3D 오브젝트(메쉬) */}
+        {/* <Box /> */}
+        <RotatingBox />
+      </Canvas>
     </div>
   );
 }
