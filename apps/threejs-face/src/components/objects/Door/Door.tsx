68259e8cf808c31ffb396bbef8c47df1e6443cc9
LeeKangbok <pmmm114@gmail.com>
2025-03-10 18:12:42 +0900

    feat: Door Animation (#18)
    [ threejs-museum.door ] feat: GSAP 라이브러리
    - @gsap/react 및 gsap 라이브러리 추가
    - tsconfig.app.json에 GSAP 타입 정의 파일 포함
    - App.tsx에서 GSAP 플러그인 등록 및 초기 설정
    
    [ threejs-museum.door ] feat: 문 상호작용 및 GSAP 애니메이션 구현
    - 문 클릭 시 GSAP를 사용한 쿼터니언 보간 애니메이션 추가
    - 문 클릭 이벤트 핸들러 구현

--- apps/threejs-face/src/components/objects/Door/Door.tsx
+++ apps/threejs-face/src/components/objects/Door/Door.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { useGLTF } from '@react-three/drei';
-import { type RapierRigidBody } from '@react-three/rapier';
+import { type RapierRigidBody, quat } from '@react-three/rapier';
 
 import RigidBody from '@/components/physics/RigidBody/RigidBody';
 
@@ -21,6 +22,96 @@ const Door = () => {
   const leftDoorRef = useRef<RapierRigidBody>(null);
   const rightDoorRef = useRef<RapierRigidBody>(null);
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  // INFO: 문 열기
+  const doorClickHandler = () => {
+    // CONDITION: 애니메이션 진행 중이면 중단
+    if (isAnimating === true) return;
+
+    // CONDITION: 문 프레임이 없으면 중단
+    if (!leftDoorRef.current || !rightDoorRef.current || !doorFrameRef.current)
+      return;
+
+    // INFO: 애니메이션 진행 시작 플래그 설정
+    setIsAnimating(true);
+
+    /**
+     * INFO: 애니메이션 지속 시간 (s)
+     */
+    const duration = 3;
+
+    /**
+     * 1) 현재(시작) 쿼터니언
+     */
+    const currentAngleQuatOfLeftDoor = quat(leftDoorRef.current.rotation());
+    const currentAngleQuatOfRightDoor = quat(rightDoorRef.current.rotation());
+    console.log(currentAngleQuatOfLeftDoor, currentAngleQuatOfRightDoor);
+
+    /**
+     * 2) 목표 쿼터니언 계산
+     *
+     * 목표 쿼터니언은 현재 쿼터니언과 목표 쿼터니언을 곱하여 얻음
+     * 목표 쿼터니언은 회전 축과 회전 각도를 포함한 쿼터니언
+     */
+    const axisY = new THREE.Vector3(0, 1, 0);
+    const angleValue = Math.PI / 2;
+    const targetQuatOfLeftDoor = quat().multiplyQuaternions(
+      currentAngleQuatOfLeftDoor,
+      quat().setFromAxisAngle(axisY, isOpen ? angleValue : -angleValue),
+    );
+    const targetQuatOfRightDoor = quat().multiplyQuaternions(
+      currentAngleQuatOfRightDoor,
+      quat().setFromAxisAngle(axisY, isOpen ? -angleValue : angleValue),
+    );
+
+    // 4) 매 프레임 보간 결과를 넣을 임시 쿼터니언(재사용)
+    const nextLeftQuat = quat();
+    const nextRightQuat = quat();
+
+    /**
+     * 3) 애니메이션 적용
+     */
+    gsap.to(
+      { t: 0 },
+      {
+        t: 1,
+        duration: duration,
+        ease: 'power2.inOut',
+        onUpdate: function () {
+          const progress = this.targets()[0].t;
+
+          /**
+           * 4) 현재 쿼터니언과 목표 쿼터니언을 보간하여 다음 쿼터니언을 계산
+           */
+          nextLeftQuat.slerpQuaternions(
+            currentAngleQuatOfLeftDoor,
+            targetQuatOfLeftDoor,
+            progress,
+          );
+
+          nextRightQuat.slerpQuaternions(
+            currentAngleQuatOfRightDoor,
+            targetQuatOfRightDoor,
+            progress,
+          );
+
+          /**
+           * 5) 쿼터니언을 설정
+           */
+          leftDoorRef.current?.setNextKinematicRotation(nextLeftQuat);
+          rightDoorRef.current?.setNextKinematicRotation(nextRightQuat);
+        },
+        onComplete: function () {
+          console.log('Complete Door Animation');
+          setIsAnimating(false);
+          setIsOpen((prev) => !prev);
+        },
+      },
+    );
+  };
+
   useEffect(() => {
     // INFO: 하위 Mesh에 Shadow 옵션 추가
     scene.traverse((child) => {
@@ -32,49 +123,55 @@ const Door = () => {
   }, [scene]);
 
   return (
-    <>
-      <group scale={[3, 3, 3]} rotation={[0, Math.PI / 2, 0]} castShadow>
-        <RigidBody ref={doorFrameRef} type="fixed" colliders="trimesh">
-          <mesh
-            geometry={nodes.Jamb_Door_0.geometry}
-            material={nodes.Jamb_Door_0.material}
-          />
-        </RigidBody>
-        <RigidBody
-          ref={leftDoorRef}
-          type="kinematicPosition"
-          colliders="hull"
-          position={DOOR_ROTATION_AXIS.left()}
-          rotation={[0, 0, 0]}
-        >
-          <mesh
-            position={
-              DOOR_ROTATION_AXIS.left().map(
-                (v) => -v,
-              ) as T.TRigidBodyComponentPosition
-            }
-            geometry={nodes.Object_18.geometry}
-            material={nodes.Object_18.material}
-          />
-        </RigidBody>
-        <RigidBody
-          ref={rightDoorRef}
-          type="kinematicPosition"
-          colliders="hull"
-          position={DOOR_ROTATION_AXIS.right()}
-        >
-          <mesh
-            position={
-              DOOR_ROTATION_AXIS.right().map(
-                (v) => -v,
-              ) as T.TRigidBodyComponentPosition
-            }
-            geometry={nodes.Object_16.geometry}
-            material={nodes.Object_16.material}
-          />
-        </RigidBody>
-      </group>
-    </>
+    <group
+      castShadow
+      scale={[3, 3, 3]}
+      rotation={[0, Math.PI / 2, 0]}
+      onClick={(e) => {
+        // INFO: 이벤트 전파 방지
+        e.stopPropagation();
+        doorClickHandler();
+      }}
+    >
+      <RigidBody ref={doorFrameRef} type="fixed" colliders="trimesh">
+        <mesh
+          geometry={nodes.Jamb_Door_0.geometry}
+          material={nodes.Jamb_Door_0.material}
+        />
+      </RigidBody>
+      <RigidBody
+        ref={leftDoorRef}
+        type="kinematicPosition"
+        colliders="hull"
+        position={DOOR_ROTATION_AXIS.left()}
+      >
+        <mesh
+          position={
+            DOOR_ROTATION_AXIS.left().map(
+              (v) => -v,
+            ) as T.TRigidBodyComponentPosition
+          }
+          geometry={nodes.Object_18.geometry}
+          material={nodes.Object_18.material}
+        />
+      </RigidBody>
+      <RigidBody
+        ref={rightDoorRef}
+        type="kinematicPosition"
+        colliders="hull"
+        position={DOOR_ROTATION_AXIS.right()}
+      >
+        <mesh
+          position={
+            DOOR_ROTATION_AXIS.right().map(
+              (v) => -v,
+            ) as T.TRigidBodyComponentPosition
+          }
+          geometry={nodes.Object_16.geometry}
+          material={nodes.Object_16.material}
+        />
+      </RigidBody>
+    </group>
   );
 };
 
