aea20b219b44ec5e960b5782e9baab4d7824220c
LeeKangbok <pmmm114@gmail.com>
2025-03-19 18:02:07 +0900

    [ threejs-museum.raycaster ] feat: Player 컴포넌트에 레이캐스트 선택 및 윤곽 효과 훅 추가
    - useRaycastSelection, useOutlineEffect
    - PerspectiveCamera에 ref 추가로 카메라 참조 관리 개선

--- apps/threejs-face/src/components/objects/Player/Player.tsx
+++ apps/threejs-face/src/components/objects/Player/Player.tsx
@@ -1,15 +1,16 @@
 import { useRef, ComponentProps } from 'react';
 import * as THREE from 'three';
-import { PointerLockControls, PerspectiveCamera } from '@react-three/drei';
+import { type ThreeElements } from '@react-three/fiber';
+import { PointerLockControls, PerspectiveCamera, Box } from '@react-three/drei';
 import type { RapierRigidBody } from '@react-three/rapier';
-import type { ThreeElements } from '@react-three/fiber';
 
 // INFO: Hooks
 import usePlayerMovement from '@/hooks/usePlayerMovement';
+import useRaycastSelection from '@/hooks/useRaycastSelector';
+import useOutlineEffect from '@/hooks/useOutlineEffect';
 
 // INFO: physics
 import RigidBody from '@/components/physics/RigidBody/RigidBody';
-
 interface IPlayerProps {
   playerRidgidBodyProps?: ComponentProps<typeof RigidBody>;
   playerMeshProps: ThreeElements['capsuleGeometry'];
@@ -21,7 +22,14 @@ const Player = ({
   playerMovementProps,
 }: IPlayerProps) => {
   const perspectiveRef = useRef<THREE.Object3D | null>(null);
+  const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
   const rigidBodyRef = useRef<RapierRigidBody>(null);
+
+  // INFO: 레이캐스트 선택 훅
+  const { selected } = useRaycastSelection({ cameraRef, distance: 5 });
+
+  // INFO: 윤곽 효과 훅
+  useOutlineEffect({ camera: cameraRef.current, activedObject: selected });
 
   // INFO: Player Movement hook
   usePlayerMovement({
@@ -52,6 +60,7 @@ const Player = ({
       </RigidBody>
       <group ref={perspectiveRef}>
         <PerspectiveCamera
+          ref={cameraRef}
           makeDefault
           fov={45}
           aspect={window.innerWidth / window.innerHeight}
