f57ad0487f6fcf0bd2f1f90a859720c920396482
LeeKangbok <pmmm114@gmail.com>
2025-05-22 16:41:20 +0900

    [ toss-stock.init ] feat: TossStock 애플리케이션 생성 및 초기 설정 추가
    - TossStock 애플리케이션을 위한 기본 파일 및 디렉토리 구조 생성

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,1 +1,10 @@
-export default ['**/*/vite.config.{ts,mts}', '**/*/vitest.config.{ts,mts}'];
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  "./apps/react-example-01/vite.config.ts",
+  "./apps/react-example-01/vite.config.codesandbox.ts",
+  "./apps/toss-stock/vite.config.ts",
+  "./libs/design-system/react/vite.config.mts",
+  "./libs/react/shadcn-ui/vite.config.mts",
+  "./libs/design-system/core/vite.config.ts"
+])
