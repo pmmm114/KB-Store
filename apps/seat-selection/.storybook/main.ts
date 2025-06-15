13cfccc108c3358ce1a0ee387dfc0536122a910c
LeeKangbok <pmmm114@gmail.com>
2025-06-15 22:39:09 +0900

    [ seat-selection.seat ]  [ seat-selection.seat ] feat: Vitest 및 Storybook 설정 추가 및 업데이트
    - Vitest 관련 설정 파일 추가 및 구성
    
    *) Storybook + Vitest Addon 통합은 이슈를 검토한 후 진행

--- apps/seat-selection/.storybook/main.ts
+++ apps/seat-selection/.storybook/main.ts
@@ -1,20 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+// import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ['../src/app/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-  addons: [],
+  stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  addons: ['@storybook/addon-onboarding'],
   framework: {
     name: '@storybook/react-vite',
-    options: {
-      builder: {
-        viteConfigPath: 'vite.config.mts',
-      },
-    },
+    options: {},
   },
 };
-
 export default config;
-
-// To customize your Vite configuration you can use the viteFinal field.
-// Check https://storybook.js.org/docs/react/builders/vite#configuration
-// and https://nx.dev/recipes/storybook/custom-builder-configs
--- apps/seat-selection/vite.config.mts
+++ apps/seat-selection/vite.config.mts
@@ -43,15 +43,4 @@ export default defineConfig({
       formats: ['es' as const],
     },
   },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/seat-selection',
-      provider: 'v8',
-    },
-  },
 });
